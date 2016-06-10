2eb8c7a7a7f60c3110e8e959290bf5fc0d13f751
Omar Daniel Leon Alvarado <leon.alvarado12@gmail.com>
2016-06-10 09:13:18 -0500

    Update BatWingArea.R

--- R/BatWingArea.R
+++ R/BatWingArea.R
@@ -10,61 +10,35 @@ BatWingArea <- function(x,scale=FALSE,method=c("BM-1988","A-1988","SS-1979","P-1
     
   }
   
-  ## Create outcome matrix
-  
-  r <- matrix(NA, 
-              nrow= length(x[,1]),
-              ncol=5)
-  
   ## Assing colum names
   
-  colnames(r) <- c("ID","BM-1988","A-1988","SS-1979","P-1977")
-  
-  for (i in 1:length(r[,1])){
-    
     ## Blood & MacFarlane 1998
     
-    BM <- 2*((x[i,2]* x[i,3]) + (0.5*(x[i,3]* x[i,5])))
-    r[i,2] <- as.numeric(BM)
+    BM <- 2*((x[,2]* x[,3]) + (0.5*(x[,3]* x[,5])))
     
     ## Aldrige 1988
     
-    A <- (2.85*(10^-3)) * x[i,6]^2/3
-    r[i,3] <- as.numeric(A)
+    A <- (2.85*(10^-3)) * x[,6]^2/3
     
     ## Smith & Starrett 1979
     
-    alpha <- atan((x[i,3]/x[i,5]))
+    alpha <- atan((x[,3]/x[,5]))
     
-    SS <- 2*( (x[i,2] * x[i,3]) + ((1/2) * (cos(alpha) * x[i,5] * x[i,4])) + ((1/2) * (sin(alpha) * x[i,4] * x[i,3])) )
-    r[i,4] <- as.numeric(SS)
+    SS <- 2*( (x[,2] * x[,3]) + ((1/2) * (cos(alpha) * x[,5] * x[,4])) + ((1/2) * (sin(alpha) * x[,4] * x[,3])) )
     
     ## Pirlot 1977
     
-    P <- 0.735 * x[i,3] * x[i,7]
-    r[i,5] <- as.numeric(P)
+    P <- 0.735 * x[,3] * x[,7]
     
-  }
+  r <- cbind(BM,A,SS,P)
   
-  r <- as.data.frame(r)
+  quote <- c("BM-1988","A-1988","SS-1979","P-1977")
   
-  ## Assing ID
+  r <- r[, which(quote%in%method)]
   
-  r$ID <- x[,1]
+  colnames(r) <- quote[which(quote%in%method)]
   
-  # Find the requested methods
-  
-  meth <- which(colnames(r)%in%method)
-  
-  n.names <- c("ID",colnames(r)[meth])
-  
-  r <- cbind(r$ID, r[,meth])
-  
-  r <- as.data.frame(r)
-  
-  colnames(r) <- n.names
-  
-  r$ID <- as.character(x[,1])
+  rownames(r) <- x[,1]
   
   ## END
   
