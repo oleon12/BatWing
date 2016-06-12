06f98255ad8f30f48d600838122c4aaf82d58bcc
Omar Daniel Leon Alvarado <leon.alvarado12@gmail.com>
2016-06-12 11:39:54 -0400

    Add files via upload

---
+++ R/dynamics.R
@@ -0,0 +1,18 @@
+#'
+#' @title
+#' Bat Wing variables for Dynamics meaures. 
+#'
+#' @description
+#' A data frame with four wing variables and ten species.
+#'
+#' @format A data.frame object with four variables (columns) and ten species (rows)
+#'
+#'
+#'
+#'@author Leon-Alvarado, Omar Daniel
+#'
+#'
+
+
+
+"dynamics"
---
+++ R/wing.R
@@ -0,0 +1,18 @@
+#'
+#' @title
+#' Bat Wing variables for LSA calculation. 
+#'
+#' @description
+#' A data frame with six variables for LSA calculation.
+#'
+#' @format A data.frame object with six variables (columns) and tens species (rows)
+#'
+#'
+#'
+#'@author Leon-Alvarado, Omar Daniel
+#'
+#'
+
+
+
+"wing"
