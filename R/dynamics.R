5cca7f3977499ccf38736a3419506d499491f7f0
Omar Daniel Leon Alvarado <leon.alvarado12@gmail.com>
2016-06-12 11:04:50 -0400

    Add files via upload

---
+++ R/dynamics.R
@@ -0,0 +1,14 @@
+#'
+#' @docType data
+#'
+#' @usage data(dynamics)
+#'
+#' @format An object of class \code{"data.frame"}
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(dynamics)
+#' 
+"dynamics"
+
---
+++ R/wing.R
@@ -0,0 +1,13 @@
+#'
+#' @docType data
+#'
+#' @usage data(wing)
+#'
+#' @format An object of class \code{"data.frame"}
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(wing)
+#' 
+"wing"
