ccd6913d8de6f697bd2fd487a0caa2697a91655d
Omar Daniel Leon Alvarado <leon.alvarado12@gmail.com>
2016-06-12 09:16:02 -0500

    Create wing.R

---
+++ R/wing.R
@@ -0,0 +1,20 @@
+#'
+#' @title
+#' A matrix with bat wing measures
+#' 
+#' @description
+#' A data as an matrix object with five terminals,
+#' following Miranda-Esquivel (2016).
+#'
+#' 
+#' @format An BASE object, class data.drame.
+#'
+#'
+#'
+#'@author Leon-Alvarado, Omar Daniel
+#'
+#'
+
+
+
+"wing"
